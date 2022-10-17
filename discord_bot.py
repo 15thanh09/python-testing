1858646f3dd31c5d5e737acd0b6c9594d4dc1b02
15thanh09 <76906673+15thanh09@users.noreply.github.com>
2022-10-17 22:32:01 +0700

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -5,7 +5,7 @@ import equation
 import requests
 import json 
 
-token = 'MTAzMTQ5MjgxNDI3OTgwNzAyNg.GkVOdw.S5lrdLf4Yu1M3mcMmeoPUMwLVKuy-1flQxh2eY'
+token = 'MTAzMTQ5MjgxNDI3OTgwNzAyNg.G5iwbF.RlClF8qEl6PnXORW6DCU53VrJ7_xllLmL5VXZA'
 client = discord.Client(intents= discord.Intents.all())
 prefix = "!"
 def parseInt(str): 
