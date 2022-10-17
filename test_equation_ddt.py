c8c63d6daf71c1afd84b1b2d241614964ee9d5b2
15thanh09 <15thanh09@gmail.com>
2022-10-17 23:26:59 +0700

    fix directory error

--- test_equation_ddt.py
+++ test_equation_ddt.py
@@ -6,7 +6,7 @@ from ddt import ddt, data, file_data, unpack
 
 @ddt
 class TestFirst(unittest.TestCase):
-    @file_data("../python-testing-main/test_equation_data.json")
+    @file_data("test_equation_data.json")
     def test_find_x(self, a, b, c, result):
         args = (a, b, c)
         self.assertEqual(equation.find_x(*args), result)
